53c0a5d1da0ca6427e79534694fe372e6bcb17a1
Satya Anudeep Kotaru <anudeepkotari1999@gmail.com>
2024-05-04 00:54:45 -0400

    2nd commit

--- app.py
+++ app.py
@@ -1,76 +1,87 @@
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, request, render_template
 import pandas as pd
+import pickle
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import base64
+from io import BytesIO
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+analytics = dict({0:0, 1:0, 2:0})
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Collect all form data
-    input_features = [x for x in request.form.values()]
-    feature_names = ['Customer_ID', 'Month', 'Age', 'Annual_Income', 'Monthly_Inhand_Salary', 
-                     'Num_Bank_Accounts', 'Num_Credit_Card', 'Interest_Rate', 'Num_of_Loan',
-                     'Delay_from_due_date', 'Num_of_Delayed_Payment', 'Changed_Credit_Limit', 'Num_Credit_Inquiries', 'Credit_Mix', 'Outstanding_Debt', 
-                     'Credit_Utilization_Ratio', 'Credit_History_Age_Months', 
-                     'Payment_of_Min_Amount', 'Total_EMI_per_month', 'Amount_invested_monthly', 
-                     'Payment_Behaviour', 'Monthly_Balance']
-
-    # Create DataFrame
-    # Collect data from the form, use None if the field is empty
-    input_features = [request.form.get(field) or None for field in feature_names]
-
-    # Create DataFrame with the correct data types
-    data = pd.DataFrame([input_features], columns=feature_names)
-
-    # Handle types and conversion (this may need to be adjusted based on the actual model requirements)
-    float_fields = ['Annual_Income', 'Monthly_Inhand_Salary', 'Outstanding_Debt',
-                    'Credit_Utilization_Ratio', 'Total_EMI_per_month', 'Amount_invested_monthly', 'Monthly_Balance']
-    int_fields = ['Age', 'Num_Bank_Accounts', 'Num_Credit_Card', 'Interest_Rate', 'Num_of_Loan',
-                  'Delay_from_due_date', 'Credit_History_Age_Months']
-
-    for field in float_fields:
-        data[field] = pd.to_numeric(data[field], errors='coerce')
-    for field in int_fields:
-        data[field] = pd.to_numeric(data[field], errors='coerce', downcast='integer')
-    
-    # Make prediction
-    result = model.predict(data)
-    y_labels = {0: "Good", 1:"Poor", 2:"Standard"}
-    print(result)
-
-    return render_template('result.html', prediction=y_labels[result[0]])
-
-
+#Load the ML Random Forest Model
 def load_model():
-    from sklearn.preprocessing import LabelEncoder
-    from sklearn.model_selection import train_test_split
-    from sklearn.ensemble import RandomForestClassifier
+    with open('random_forest_model.pkl', 'rb') as file:
+        model = pickle.load(file)
+    return model
+model = load_model()
 
-    train_data = pd.read_csv('clean_train_data.csv')
-    # Encode the Customer_ID column
-    label_encoder = LabelEncoder()
-    train_data['Customer_ID'] = label_encoder.fit_transform(train_data['Customer_ID'])
+#Plot the analytics graph
+def plot_analytics():
+    file_path = './static/plot.png'
 
-    X = train_data.drop(columns=['Credit_Score'])
-    y = train_data['Credit_Score']
+    labels = ['Good', 'Poor', 'Standard']
+    counts = [analytics[0], analytics[1], analytics[2]]
+    plt.plot(labels, counts, marker='o')
+    plt.xlabel('Prediction Outcome')
+    plt.ylabel('Counts')
+    plt.title('Prediction Analytics')
+    plt.savefig(file_path,  dpi=300)
+    plt.close()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
 
-    print("Training set shape:", X_train.shape, y_train.shape)
-    print("Testing set shape:", X_test.shape, y_test.shape)
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Collect all form data
+        input_features = [x for x in request.form.values()]
+        feature_names = ['Customer_ID', 'Month', 'Age', 'Annual_Income', 'Monthly_Inhand_Salary', 
+                        'Num_Bank_Accounts', 'Num_Credit_Card', 'Interest_Rate', 'Num_of_Loan',
+                        'Delay_from_due_date', 'Num_of_Delayed_Payment', 'Changed_Credit_Limit', 'Num_Credit_Inquiries', 'Credit_Mix', 'Outstanding_Debt', 
+                        'Credit_Utilization_Ratio', 'Credit_History_Age_Months', 
+                        'Payment_of_Min_Amount', 'Total_EMI_per_month', 'Amount_invested_monthly', 
+                        'Payment_Behaviour', 'Monthly_Balance']
 
-    random_forest = RandomForestClassifier(n_estimators=100, min_samples_leaf=1, min_samples_split=2)
-    random_forest.fit(X_train, y_train)
+        # Create DataFrame from the form, use None if the field is empty
+        input_features = [request.form.get(field) or None for field in feature_names]
 
-    print('Model loaded successfully...!!')
+        # Create DataFrame with the correct data types
+        data = pd.DataFrame([input_features], columns=feature_names)
 
-    return random_forest
+        # Handle categorical values
+        credit_mix = {'good':0, 'standard':1, 'other':2}
+        data['Credit_Mix'] = credit_mix[data['Credit_Mix'][0]]
 
+        # Handle types and conversion
+        float_fields = ['Annual_Income', 'Monthly_Inhand_Salary', 'Outstanding_Debt',
+                        'Credit_Utilization_Ratio', 'Total_EMI_per_month', 'Amount_invested_monthly', 'Monthly_Balance']
+        int_fields = ['Age', 'Num_Bank_Accounts', 'Num_Credit_Card', 'Interest_Rate', 'Num_of_Loan',
+                    'Delay_from_due_date', 'Credit_History_Age_Months']
+
+        for field in float_fields:
+            data[field] = pd.to_numeric(data[field], errors='coerce')
+        for field in int_fields:
+            data[field] = pd.to_numeric(data[field], errors='coerce', downcast='integer')
+        
+        #Encoding results to labels
+        y_labels = {0: "Good", 1:"Poor", 2:"Standard"}
+        result = model.predict(data)
+
+        analytics[result[0]] += 1
+        plot_analytics()
+        prediciton = y_labels[result[0]]
+        print(result)
+
+        #Return the results to the website
+        return jsonify({'prediction': prediciton})
+    
+    else:
+        return render_template('index_layout.html')
 
 
 if __name__ == '__main__':
-    model = load_model()
-    app.run(port=1234)
+    app.run()
